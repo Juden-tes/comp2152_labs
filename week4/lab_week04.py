5cfba9e7a467f7eabccc0e0001a88d9e19f70caf
Judene Brown <judenebrown13@gmail.com>
2025-01-31 11:11:17 -0500

    add question

--- week4/lab_week04.py
+++ week4/lab_week04.py
@@ -81,6 +81,14 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+#define the monster power
+
+monster_power={
+    "Fire magic":2,
+    "Freezing Time":4,
+    "Super Hearing":6,
+}
+
 # Define the number of stars awarded to the Player
 num_stars = 0
 
@@ -150,6 +158,10 @@ print("--- You are matched in strength: " + str(combat_strength == m_combat_stre
 
 # Check the Player's overall strength and health
 print("--- You have a strong player: " + str((combat_strength + health_points) >= 15))
+
+#lab04 -question2 
+input("Roll for the monster power (Press Enter key)")
+power_roll= random.choice(["Fire Magic","Frezzing Time","Super Hearing"])
 
 # Loop while the monster and the player are alive. Call fight sequence functions
 print("You meet the monster. FIGHT!!")
